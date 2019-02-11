5f33a938c7fa36782296648489f86c614c0c0e8d
Julie JAUMARY <jaumary.julie@gmail.com>
2019-02-11 15:09:46 +0100

    Exo 1

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# Super commentaire
+class QuestionsController < ApplicationController
+  # def ask
+  #   @ask = params[:question]
+  # end
+
+  def answer
+    ask = params[:question]
+    @answer = if ask == 'I am going to work'
+                'Great!'
+              elsif ask.include?('?')
+                'Silly question, get dressed and go to work!'
+              else
+                'I don\'t care, get dressed and go to work!'
+              end
+  end
+end
